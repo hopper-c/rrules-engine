a399cbe8d278bfc665c3a5a370af1300610ca84b
hopper-c <hopper.c.t@gmail.com>
2022-10-27 15:30:40 -0500

    Added fn variable and some test code

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
-enum Operator {
-    lessThan,
-    lessThanEqualTo,
-    greaterThan,
-    greaterThanEqualTo,
-    equal,
-    
-}
+use serde_json::{ Value, json} ;
 
-struct Condition {
+#[derive(Debug)]
+pub struct Condition {
     predicate: String,
-    operator: Operator,
+    operator: String,
     fact: String
 }
 
 #[derive(Debug)]
-struct Rule { 
+pub struct Rule { 
     priority: u32,
-    condition: Vec<String>,
-    operator: String,
-    fact: String,
-    action: || -> ()
+    conditions: Vec<Condition>,
+    action: fn(Value) -> bool
 }
 
 fn main() {
-    let first_rule = Rule {
-        priority: 1,
-        condition: Vec<String> { "Stuff" },
-        operator: "=",
-        fact: "Stuff"
+
+    let mut conditions: Vec<Condition> = Vec::new();
+
+    let condition = Condition {
+        predicate: String::from("Predicate"), 
+        operator: String::from("operator"), 
+        fact: String::from("fact")
     };
 
+    conditions.push(condition);
+
+    let john = json!({
+        "name": "John Doe",
+        "age": 43,
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    });
+
+    let first_rule = Rule {
+        priority: 1,
+        conditions: conditions,
+        action: |value: Value| -> bool {
+            let mut is_true = false;
+
+            if value["name"] == "John Doe"{
+                is_true = true;
+            }
+
+            return is_true;
+        }
+    };
+
+    let is_activated = first_rule.action;
+    println!("{}", is_activated(john));
 }
